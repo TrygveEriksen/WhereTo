07164d679069269f3d82ae9b4f5d176c177a6258
eliashet <eliashet@stud.ntnu.no>
2024-02-01 15:46:28 +0100

    feat: set up mongodb atlas
    made account on atlas, configured the db, and conncted the backend to it

--- backend/index.js
+++ backend/index.js
@@ -7,7 +7,14 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-mongoose.connect("mongodb://127.0.0.1:27017/TDT4140");
+const dbUsername = "whereto72";
+const dbPass = "kOIUq6hG3qlEWXiU";
+const dbName = "whereto";
+
+const url = `mongodb+srv://${dbUsername}:${dbPass}@cluster0.nzo0zib.mongodb.net/${dbName}`;
+mongoose.connect(url)
+  .then(()=> console.log("connected to db"))
+  .catch((err)=>console.error("error connecting to db", err))
 
 app.get("/destinations", async (req, res) => {
   try {
