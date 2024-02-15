50c6bcf21f283b8c01a7220729918e0c962f85b7
Iselin Remaut Lund <you@example.comiselinlu@ntnu.no>
2024-02-15 18:33:35 +0100

    #40 added stared places componentent

--- TravelPlanner/src/Components/Mypage/Mypage.jsx
+++ TravelPlanner/src/Components/Mypage/Mypage.jsx
@@ -7,6 +7,7 @@ import DeleteIcon from "@mui/icons-material/Delete";
 import SendIcon from "@mui/icons-material/Send";
 import Stack from "@mui/material/Stack";
 import myImage from "./Picture/myprofile.png";
+import StaredPlaces from "./StaredPlaces/StaredPlaces";
 
 function Mypage() {
   const [user, setUser] = useState("");
@@ -23,20 +24,23 @@ function Mypage() {
   return (
     <>
       <Navbar />
-      <div className="divBox">
-        <div className="divMypage">
-          <h1 className="myPage">Min side</h1>
-          <img
-            src={myImage}
-            alt="My profile picture"
-            className="profile-image"
-          ></img>
-          <h3>Brukernavn: {user}</h3>
-          <p>Dette er min bruker side mere info kommer i neste sprint</p>
-          <Button variant="outlined" size="small" startIcon={<DeleteIcon />}>
-            Slett
-          </Button>
+      <div className="myPageContainer">
+        <div className="divBox">
+          <div className="divMypage">
+            <h1 className="myPage">Min side</h1>
+            <img
+              src={myImage}
+              alt="My profile picture"
+              className="profile-image"
+            ></img>
+            <h3>Brukernavn: {user}</h3>
+            <p>Dette er min bruker side mere info kommer i neste sprint</p>
+            <Button variant="outlined" size="small" startIcon={<DeleteIcon />}>
+              Slett
+            </Button>
+          </div>
         </div>
+        <StaredPlaces />
       </div>
     </>
   );
---
+++ TravelPlanner/src/Components/Mypage/StaredPlaces/StaredPlaces.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import "./StaredPlaces.css";
+
+function StaredPlaces() {
+  return (
+    <div>
+      <h1>Steder jeg har vært</h1>
+      <ul>
+        <li>Sted 1</li>
+        <li>Sted 2</li>
+        <li>Sted 3</li>
+      </ul>
+    </div>
+  );
+}
+
+export default StaredPlaces;
