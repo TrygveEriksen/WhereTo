eda7a63d2b17e1b1c39852b575ea58957e644a05
johannedn <johannedn00@gmail.com>
2024-02-15 13:24:37 +0100

    Laget component og side, koblet til navbaren
    Co-authored-by: IselinRemaut <IselinRemaut@users.noreply.github.com>

---
+++ TravelPlanner/src/Components/Mypage/Mypage.jsx
@@ -0,0 +1,17 @@
+import Navbar from "../Navbar/Navbar"
+
+function Mypage() {
+  return (
+	<>
+	<Navbar />
+    <div>
+		<h1>Hei Kari</h1>
+		<p>Dette er din brukerside</p>
+		<p>Her vil det etterhvert vært mulig å slette brukeren din</p>
+    </div>
+	</>
+  )
+
+}
+
+export default Mypage
