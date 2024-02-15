54c65804efbc8b34c098057fa25148d4235ec9aa
eliashet <eliashet@stud.ntnu.no>
2024-02-15 17:27:36 +0100

    feat: add destination not found on invalid search

--- TravelPlanner/src/Components/Home/Home.jsx
+++ TravelPlanner/src/Components/Home/Home.jsx
@@ -11,6 +11,7 @@ function Home() {
   const [destinations, setDestinations] = useState([]);
   const [visibleDestinations, setVisibleDestinations] = useState([]);
   const [isLoading, setLoading] = useState(true);
+  const [searchWord, setSearchWord] = useState("");
 
   //fetch data from the server when the page starts running and set it in the state destination
   useEffect(() => {
@@ -28,36 +29,48 @@ function Home() {
 
   const handleSearch = (e) => {
     e.preventDefault();
-    const searchWord = e.target.value;
-    if (searchWord === "") {
+    const sWord = e.target.value;
+    setSearchWord(sWord);
+    window.scrollTo(0, 0);
+    if (sWord === "") {
       setVisibleDestinations(destinations);
+    } else {
+      setVisibleDestinations(
+        destinations.filter((destination) => {
+          return (
+            destination.place
+              .toLowerCase()
+              .includes(sWord.toLowerCase()) ||
+            destination.country
+              .toLowerCase()
+              .includes(sWord.toLowerCase()) ||
+            destination.continent
+              .toLowerCase()
+              .includes(sWord.toLowerCase())
+          );
+        })
+      );
     }
-    else {
-      setVisibleDestinations(destinations.filter((destination) => {
-        return (destination.place.toLowerCase().includes(searchWord.toLowerCase()) 
-        || destination.country.toLowerCase().includes(searchWord.toLowerCase())
-        || destination.continent.toLowerCase().includes(searchWord.toLowerCase()))
-      }));
-    }
-
-
   };
-
 
   if (visibleDestinations.length === 0) {
     window.scrollTo(0, 0);
   }
-
 
   return (
     <>
       <Navbar />
       <div className="homeContainer">
         <div className="homeContent">
-          
           {isLoading && <Loading />}
           <div className="searchBox">
-            <input className="searchField" type="text" onChange={handleSearch} autoFocus></input>
+            <input
+              className="searchField"
+              type="text"
+              onChange={handleSearch}
+              value={searchWord}
+              autoFocus
+            ></input>
           </div>
           <ul className="destinations">
             {visibleDestinations.map((destination) => (
@@ -71,6 +84,14 @@ function Home() {
                 </Link>
               </li>
             ))}
+            {visibleDestinations.length === 0 && (
+              <li className="oneDestination">
+                <a className="destAnchor">
+                  <p className="destLink dest1">Ingen resultater matcher:</p>
+                  <p className="destLink dest2">{"<"} <span className="noResults">{searchWord}</span>{">"}</p>
+                </a>
+              </li>
+            )}
           </ul>
         </div>
       </div>
@@ -78,5 +99,5 @@ function Home() {
     </>
   );
 }
- 
+
 export default Home;
