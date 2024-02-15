153df17a8f1b0edee76f3d9bb6596e1cc38e715e
Iselin Remaut Lund <you@example.comiselinlu@ntnu.no>
2024-02-15 18:15:43 +0100

    #33 added a picture and styled my page

--- TravelPlanner/src/Components/Mypage/Mypage.jsx
+++ TravelPlanner/src/Components/Mypage/Mypage.jsx
@@ -6,6 +6,7 @@ import Button from "@mui/material/Button";
 import DeleteIcon from "@mui/icons-material/Delete";
 import SendIcon from "@mui/icons-material/Send";
 import Stack from "@mui/material/Stack";
+import myImage from "./Picture/myprofile.png";
 
 function Mypage() {
   const [user, setUser] = useState("");
@@ -24,11 +25,16 @@ function Mypage() {
       <Navbar />
       <div className="divBox">
         <div className="divMypage">
-          <h1>My account</h1>
-          <h3>Username: {user}</h3>
-          <p>This is my userpage</p>
+          <h1 className="myPage">Min side</h1>
+          <img
+            src={myImage}
+            alt="My profile picture"
+            className="profile-image"
+          ></img>
+          <h3>Brukernavn: {user}</h3>
+          <p>Dette er min bruker side mere info kommer i neste sprint</p>
           <Button variant="outlined" size="small" startIcon={<DeleteIcon />}>
-            Delete
+            Slett
           </Button>
         </div>
       </div>
