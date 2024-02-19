239f08b195b2c4dc3a8ee39db3dab98f77135ddf
johannedn <johannedn00@gmail.com>
2024-02-19 12:13:04 +0100

    Lagt til ikoner til linkene, og endrer farge when hover

--- TravelPlanner/src/Components/Navbar/Navbar.jsx
+++ TravelPlanner/src/Components/Navbar/Navbar.jsx
@@ -21,24 +21,29 @@ function Navbar(){
 		<nav className="navbar">
 			<ul className="navbar-nav">
 				<li className="nav-item">
-					<Link to="/"> 
-						<img src={'/images/logo.svg'} alt="logo" id='nav-logo'/> 
-						<span>Where to?</span>
+					<Link to="/" className='nav-link'> 
+						<img src='/images/SVG/logo_home.svg' alt="logo" className='nav-logo' height="50px"/>
 					</Link>
 				</li>
-				{permission == 1?<li >
-					<Link to="/newdestination">
-						Jeg er admin
+				{permission == 1?<li className='nav-item'>
+					<Link to="/newdestination" className='nav-link'>
+						<img src='/images/SVG/admin2.svg' alt="admin" className='nav-logo' height="60px"/>
+						<br />
+						<span className='nav-text'>Admin</span>
 					</Link>
 					</li>:null}
 				<li className="nav-item">
-					<Link to= "/myPage">
-						Mypage
+					<Link to= "/myPage" className='nav-link'>
+						<img src='/images/SVG/mypage.svg' alt="my_page" className='nav-logo' height="60px"/>
+						<br />
+						<span className='nav-text'>Min side</span>
 					</Link>
 				</li>
 				<li>				
-					<Link to="/login" className="signout" onClick={() => localStorage.removeItem("user")}>
-						Logg ut
+					<Link to="/login" className="signout nav-link" onClick={() => localStorage.removeItem("user")}>
+						<img src='/images/SVG/logout.svg' alt="log_out" className='nav-logo' height="60px"/>
+						<br />
+						<span className='navText'>Logg ut</span>
 					</Link>
 				</li>
 
