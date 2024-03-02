653254970842880e84928a801b54fecc1cd9f9b1
Trygve Eriksen <tryger@stud.ntnu.no>
2024-03-02 15:44:41 +0100

    feat: add button and functionality for marking visited places

--- backend/controllers/entryController.js
+++ backend/controllers/entryController.js
@@ -42,6 +42,24 @@ const signUpUser = async (req, res) => {
   }
 };
 
+const toggleVisited = async (req, res) => {
+  try {
+    const user = await UserModel.findOne({ _id: req.body.userId });
+    if (!user) {
+      return res.status(404).json([]);
+    }
+    if (user.visited.includes(req.body.id)) {
+      user.removeVisited(req.body.id);
+      const result = await user.save();
+    } else {
+      user.addVisited(req.body.id);
+      const result = await user.save();
+    }
+    return res.json({ message: "success" });
+  }
+  catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+}
 
-
-module.exports = { loginUser, signUpUser };
+module.exports = { loginUser, signUpUser, toggleVisited };
