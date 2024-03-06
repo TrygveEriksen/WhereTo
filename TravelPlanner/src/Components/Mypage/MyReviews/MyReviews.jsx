99e7814285d5051752ec3095561406af31459d92
Iselin Remaut Lund <you@example.comiselinlu@ntnu.no>
2024-03-06 20:43:56 +0100

    #59 created a seperate component for editing reviews

--- TravelPlanner/src/Components/Mypage/MyReviews/MyReviews.jsx
+++ TravelPlanner/src/Components/Mypage/MyReviews/MyReviews.jsx
@@ -1,17 +1,12 @@
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import "../Mypage.css";
 import "./MyReviews.css";
 import { API } from "../../../API/API";
 import Loading from "../../Loading/Loading";
 import StarRating from "../../StarRating/StarRating";
 import EditIcon from "@mui/icons-material/Edit";
-import Button from "@mui/material/Button";
-import Dialog from "@mui/material/Dialog";
-import DialogContent from "@mui/material/DialogContent";
-import DialogContentText from "@mui/material/DialogContentText";
-import DialogTitle from "@mui/material/DialogTitle";
 import "../../Descriptions/NewReview/NewReview.css";
-import ClearIcon from "@mui/icons-material/Clear";
+import EditReview from "./EditReview";
 
 function MyReviews(props) {
   const [reviews, setReviews] = useState([]);
@@ -86,7 +81,7 @@ function MyReviews(props) {
   };
   useEffect(() => {
     load();
-  }, [props.userId, reviewToEdit, stars, comment, title]);
+  }, [props.userId]);
 
   const load = async () => {
     if (props.userId) {
@@ -125,117 +120,19 @@ function MyReviews(props) {
             </div>
           </li>
         ))}
-        {/* <EditReview {...{ open, handleClose, handleDelete, reviewToEdit }} /> */}
-        <Dialog open={open} onClose={handleClose}>
-          <ClearIcon onClick={handleClose} />
-          <DialogTitle>Rediger din vurdering</DialogTitle>
-          <DialogContent>
-            <DialogContentText>
-              <form onSubmit={handleSubmit} className="newReviewForm">
-                <div className="starRating">
-                  <input
-                    type="radio"
-                    id="star1"
-                    name="rating"
-                    className="starCheckbox"
-                    value="1"
-                    onChange={handleStars}
-                    checked={false}
-                  />
-                  <label
-                    htmlFor="star1"
-                    className={"fa fa-star " + (stars > 0 ? "filled" : "empty")}
-                  ></label>
-                  <input
-                    type="radio"
-                    id="star2"
-                    name="rating"
-                    className="starCheckbox"
-                    value="2"
-                    onChange={handleStars}
-                    checked={false}
-                  />
-                  <label
-                    htmlFor="star2"
-                    className={"fa fa-star " + (stars > 1 ? "filled" : "empty")}
-                  ></label>
-                  <input
-                    type="radio"
-                    id="star3"
-                    name="rating"
-                    className="starCheckbox"
-                    value="3"
-                    onChange={handleStars}
-                    checked={false}
-                  />
-                  <label
-                    htmlFor="star3"
-                    className={"fa fa-star " + (stars > 2 ? "filled" : "empty")}
-                  ></label>
-                  <input
-                    type="radio"
-                    id="star4"
-                    name="rating"
-                    className="starCheckbox"
-                    value="4"
-                    onChange={handleStars}
-                    checked={false}
-                  />
-                  <label
-                    htmlFor="star4"
-                    className={"fa fa-star " + (stars > 3 ? "filled" : "empty")}
-                  ></label>
-                  <input
-                    type="radio"
-                    id="star5"
-                    name="rating"
-                    className="starCheckbox"
-                    value="5"
-                    onChange={handleStars}
-                    checked={false}
-                  />
-                  <label
-                    htmlFor="star5"
-                    className={"fa fa-star " + (stars > 4 ? "filled" : "empty")}
-                  ></label>
-                </div>
-
-                <label className="descriptionLabel" htmlFor="title">
-                  Tittel:
-                </label>
-                <input
-                  type="text"
-                  id="title"
-                  placeholder="Tittel"
-                  value={title}
-                  onChange={handleTitleChange}
-                  className="newReviewInput"
-                ></input>
-
-                <label className="descriptionLabel" htmlFor="description">
-                  Beskrivelse:
-                </label>
-                <textarea
-                  id="description"
-                  placeholder="Beskriv din erfaring med dette stedet"
-                  value={comment}
-                  onChange={handleCommentChange}
-                  className="descriptionInput"
-                  rows={5}
-                ></textarea>
-                {/* <p>{reviewToEdit.stars}</p> */}
-
-                <input type="submit" className="submitBtn" value="Oppdater" />
-                <input
-                  type="submit"
-                  className="submitBtn"
-                  value="Slett"
-                  onClick={() => handleDelete(reviewToEdit._id)}
-                />
-              </form>
-            </DialogContentText>
-          </DialogContent>
-        </Dialog>
+        <EditReview
+          open={open}
+          handleClose={handleClose}
+          handleStars={handleStars}
+          handleTitleChange={handleTitleChange}
+          handleCommentChange={handleCommentChange}
+          handleSubmit={handleSubmit}
+          handleDelete={handleDelete}
+          stars={stars}
+          title={title}
+          comment={comment}
+          reviewToEdit={reviewToEdit}
+        />
       </ul>
     </div>
   );
